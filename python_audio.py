d28b13c47acb81a9f29eb44a9d91806ad30c9e3e
pankid <nick@nschoonover.com>
2015-08-11 22:31:22 -0400

    added guessing

--- python_audio.py
+++ python_audio.py
@@ -2,8 +2,16 @@
 
 import os
 import random
-tone=random.randrange(50,17000)
+tone=random.randrange(50,8000)
 print tone
 play_synth = "play -n synth .5 sine %s fade h 0.1 1 0.1" % (tone)
 os.system(play_synth)
 
+guess_tone = int(raw_input("guess the tone:"))
+
+if guess_tone == tone:
+	print "correct"
+	print "%sHz" % (tone)
+else:
+	print "no"
+	print "%sHz" % (tone)
